543fcdad2362e9bd303e3df494bea050b928d604
khlin <khlin@thinktronltd.com>
2020-11-22 13:00:05 +0800

    adding higchart

--- assets/js/initial.js
+++ assets/js/initial.js
@@ -39,6 +39,16 @@ $(document).ready(function(){
           }
         }).addTo(map)
         console.log(html.initialMainPageHtml)
+        var chart = any_series_chart_render_two_panes({
+          data: [html.initialMainPageHtml.output.seriesChart.economic],
+          dataRain: [html.initialMainPageHtml.output.seriesChart.rain],
+          renderdiv: 'chart',
+          plot_type: ["column"],
+          dashStyle: [null],
+          series_name: ["事件農業損失總計"],
+          series_color: ["red"],
+          y_title: "經濟損失(萬)"
+        })
       }, 1000)
     } else {
 
@@ -50,12 +60,18 @@ $(document).ready(function(){
 })
 
 function mainPageTemplate(data){
+  console.log(data)
   var input = {
     event: data.project.length,
     economic: 0,
     region: 0,
     county: {},
-    agriType: 0
+    agriType: 0,
+    seriesChart: {
+      eventName: [],
+      economic: [],
+      rain: []
+    }
   }
   data.project.forEach(function(element){
     input.economic += element.totalEconomicLose / 10000
@@ -65,6 +81,11 @@ function mainPageTemplate(data){
       input.county[eachRegion.name] += 1
       input.agriType += eachRegion.crops.length
     })
+    // seriesChart
+    var thisEventTimeUtc = dateStringToUtc(element.event.start)
+    input.seriesChart.eventName.push([thisEventTimeUtc, element.name])
+    input.seriesChart.economic.push([thisEventTimeUtc, element.totalEconomicLose])
+    input.seriesChart.rain.push([thisEventTimeUtc, element.estimatedRainAccumulation])
   })
   var html = `
     <section class="counts pb-0" style="height: calc( 100vh - 125px ); padding-top: 10px;">
@@ -100,7 +121,7 @@ function mainPageTemplate(data){
                   <p>損失作物種類</p>
                 </div>
               </div>
-              <div class="col-lg-12 mt-2" style="height: calc( 100vh - 300px )">
+              <div class="col-lg-12 mt-2" id="chart" style="height: calc( 100vh - 300px )">
               </div>
             </div>
           </div>
@@ -157,8 +178,148 @@ function ajax_call(url){
   return result;
 }
 
+function dateStringToUtc(value){
+  var result,
+    year = parseInt(value.slice(0,4)),
+    month = parseInt(value.slice(4,6))-1,
+    date = parseInt(value.slice(6,9))
+  result = Date.UTC(year, month, date);
+  return result;
+}
+
 function modal_open(input){
   $("#atrr-info-modal .modal-title").html(input.title);
   $("#atrr-info-modal .modal-body").html(input.content);
   $("#atrr-info-modal").modal();
+}
+
+function any_series_chart_render_two_panes(input_data){
+	var series_array_input = []
+	if(!input_data.time_label){ input_data.time_label = 24 * 3600 * 1000 }
+	if(!input_data.dashStyle){ input_data.dashStyle = [] }
+	if(!input_data.xAxis_label){ input_data.xAxis_label = 16 }
+	input_data.data.forEach(function(element, index){
+		if(!input_data.dashStyle){ input_data.dashStyle.push(0) }
+		series_array_input.push({
+			type: input_data.plot_type[index],
+			dashStyle: input_data.dashStyle[index],
+			name: input_data.series_name[index],
+			color: input_data.series_color[index],
+			yAxis: 1,
+			data: element,
+			tooltip: { valueDecimals: 0}
+    })
+  })
+  series_array_input.push({
+    type: 'column',
+    name: '事件估計累積降雨量',
+    color: 'blue',
+    yAxis: 0,
+    data: input_data.dataRain[0],
+    tooltip: { valueDecimals: 0}
+  })
+	var chart = Highcharts.stockChart({
+		chart: {
+			zoomType: 'x',
+			renderTo: input_data.renderdiv,
+			backgroundColor: 'white',
+		},
+		title: {
+			text: input_data.title
+		},
+		rangeSelector: {
+			selected: 3,
+			enabled: false,
+			buttons: [{
+				type: 'hour',
+				count: 6,
+				text: '6h'
+			}, {
+				type: 'hour',
+				count: 12,
+				text: '12h'
+			}, {
+				type: 'day',
+				count: 1,
+				text: '1d'
+			}, {
+				type: 'day',
+				count: 2,
+				text: '2d'
+			}, {
+				type: 'month',
+				count: 1,
+				text: '1m'
+			}, {
+				type: 'all',
+				count: 1,
+				text: 'All'
+			}]
+		},
+		legend: {
+			enabled: true
+		},
+		navigator: {
+			enabled: false
+		},
+		scrollbar: {
+            enabled: false
+        },
+		plotOptions: {
+	        series: {
+	            dataGrouping: {
+	                enabled: false
+				},
+				showInNavigator: true
+	        }
+		},
+		series: series_array_input,
+    tooltip: {
+      xDateFormat: '%Y-%m-%d %H:%M'
+    },
+		xAxis: {
+      dateTimeLabelFormats: {
+				millisecond: '%H:%M:%S.%L',
+				second: '%H:%M:%S',
+				minute: '%H:%M',
+				hour: '%H:%M',
+				day: '%m/%e',
+				week: '%e. %b',
+				month: '%Y/%m',
+				year: '%Y'
+      }
+		},
+		yAxis: [
+			{
+				labels: {
+					align: 'left',
+					x: -20
+				},
+				showLastLabel: true,
+				height: '30%',
+				title: {
+					text: "累積雨量(mm)",
+          y: 60,
+          x: -10
+				},
+				lineWidth: 2,
+				opposite: false,
+				reversed: true
+			}, {
+				labels: {
+					align: 'right',
+					x: -3
+        },
+        height: '60%',
+        top: '40%',
+				title: {
+					  text: input_data.y_title
+				},
+				lineWidth: 2,
+				max: input_data.max,
+				plotLines: input_data.critical_line
+			}
+		]
+	})
+	return chart
 }
